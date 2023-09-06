6148bc2754c58a5e50b451bbeac5ad022a5908c8
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-06 17:38:57 +0530

    upload my cpp projects

--- Number.cpp
+++ Number.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-main(){
-    int x=3, y=8, z;
-    z=x+y;
-    printf("%d", z);
+int main()
+{
+    int x = 8;
+    x++;
+    cout << 5 + 7 << endl;
+    cout << x << endl;
+    cout << pow(2, 3) << endl;
+    cout << pow(2.3, 2) << endl;
+    cout << sqrt(1466664) << endl;
+    cout << round(4.5) << endl;
+    cout << ceil(5.1) << endl;
+    cout << floor(6.9) << endl;
+    cout << fmax(5, 8) << endl;
+
+    return 0;
 }
