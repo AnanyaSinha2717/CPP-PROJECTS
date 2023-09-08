f6f7a270148f64096996b16636b412e45d4f4281
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-08 12:06:33 +0530

    upload my cpp projects

---
+++ Array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int nums[] = {1, 3 , 4, 5, 5};
+    cout << nums[0] << endl;
+
+    return 0;
+}
---
+++ Functions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+void sayhi(string name){
+    cout << "hello " << name << endl;
+}
+
+int main(){
+    sayhi("Mike");
+    return 0;
+
+}
