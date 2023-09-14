369938eab2c8fe7c8945f462f9a1b0fc63b2096b
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-14 13:50:53 +0530

    Pattern with equal rows and columns.

---
+++ pattern-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int i = 1;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << endl;
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
