fa3071264ccf3343407b31637eb583136556118d
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-14 14:19:57 +0530

    square pattern with numbers preceeding in a row.

---
+++ 4-pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int i = 1;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << endl;
+
+    while (i <= n) // creating columns
+    {
+        int j = n;
+        while (j != 0) // creating a row
+        {
+            cout << j << " ";
+            j--;
+        }
+        cout << endl;
+        i++;
+    }
+}
