8bd06653a3175c7b56b8701cfd11e85f3c0ffc46
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-16 18:31:01 +0530

    square pattern with alphabets succeeding in a row

---
+++ Patterns/10.cpp
@@ -0,0 +1,31 @@
+// square matrix of alphabets
+// ASCII 65 - 90 for A - Z
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int i = 1;
+
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << endl;
+
+    while (i <= n)
+    {
+        int j = 1;
+        char value = 'A';
+        while (j <= n)
+        {
+            cout << value << " ";
+            value++;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
