51a6c45c38578848f95d4755287608765f728135
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-21 20:51:33 +0530

    format document

--- Patterns/14.cpp
+++ Patterns/14.cpp
@@ -26,7 +26,7 @@ int main()
             cout << "* ";
             j++;
         }
-        
+
         cout << endl;
         i++;
     }
--- Patterns/15.cpp
+++ Patterns/15.cpp
@@ -13,7 +13,7 @@ int main()
     while (i <= n)
     {
         int j = 1;
-        while (j <= n-i+1)
+        while (j <= n - i + 1)
         {
             cout << "* ";
             j++;
