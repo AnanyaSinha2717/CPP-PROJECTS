435ae1d70b48af8109c721b11f4928b2611c3c4a
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-21 20:20:16 +0530

    inverted star triangle pattern

---
+++ Patterns/15.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n-i+1)
+        {
+            cout << "* ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
