1efcc7d0d6f3fd019b89114afdaeb6016e03474c
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-09-21 20:14:46 +0530

    square matrix with star and spaces

---
+++ Patterns/14.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (i <= n)
+    {
+        int space = n - i;
+        int j = 1;
+
+        while (space)
+        {
+            cout << "  ";
+            space--;
+        }
+        while (j <= i)
+        {
+            cout << "* ";
+            j++;
+        }
+        
+        cout << endl;
+        i++;
+    }
+}
