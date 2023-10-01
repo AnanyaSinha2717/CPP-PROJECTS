80a0210a3548773e0c4e4592a3b2f2877047bc04
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-10-01 15:25:02 +0530

    some trials with switch() function

---
+++ INDIE/switch_try.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+// #include <cmath>
+
+// using namespace std;
+
+// int main()
+// {
+//     char ch = '1';
+//     int num = 2;
+//     while (ch == '1')
+//     {
+//         cout << "while loop" << endl;
+
+//         switch (num)
+//         {
+//         case 1:
+//             cout << "first" << endl;
+//             break;
+//         default: // not mandatory, the program will just end without it
+//             cout << "default" << endl;
+//             break;
+//         }
+//     }
+//     return 0;
+// }
