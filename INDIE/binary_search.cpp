bca7222db874607edc00bd953136367f7c58fdab
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-11-02 14:17:56 +0530

    binary search in odd and even array

---
+++ INDIE/binary_search.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+    int start = 0, end = size - 1;
+    int mid = (start + end) / 2;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (arr[mid] > key)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+        mid = (start+end)/2;
+    }
+    return -1;
+}
+
+int main()
+{
+    int even[6] = {2, 4, 6, 8, 12, 18};
+    int odd[5] = {3, 8, 11, 14, 16};
+
+    int index = binarySearch(even, 6, 6);
+    int index2 = binarySearch(odd, 5, 9);
+
+    cout << "in even array = " << index << ", " << "in odd array = " << index2 << endl;
+    return 0;
+}
