2cbfa86a424a634bbded717e2fa255c902208bf1
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-11-09 23:21:34 +0530

    pivot index

---
+++ INDIE/pivot.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int pivotIndex(int arr[], int n){
+    int s = 0, e = n-1;
+    while(s<e)
+    {
+        int mid = s+(e-s)/2;
+        if (arr[mid] >= arr[0])
+        {
+            s = mid+1;
+        }
+        else{
+            e = mid;
+        }
+    }
+    return s;
+}
+
+int main(){
+    int arr[5] = {7,8,1,2,3};
+
+    int pivot = pivotIndex(arr, 5);
+    cout << "Pivot index is " << pivot << endl;
+
+    return 0;
+}
