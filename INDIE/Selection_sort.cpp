6c02aea5028609cb37769284e6784217f7ee0bf8
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2023-12-01 23:55:32 +0530

    needs improvement

---
+++ INDIE/Selection_sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int size;
+    cout << "Size of array: ";
+    cin >> size;
+    int arr[size];
+
+    cout << "Enter the elements of the array:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+    printArray(arr, size);
+
+    return 0;
+}
